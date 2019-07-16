4ce860eabcaec5ef326b8604dbbe9f402cb06597
liyu <980531752@qq.com>
2019-07-16 22:10:18 +0800

    add test project

--- test/test.cpp
+++ test/test.cpp
@@ -1,21 +1,55 @@
 ﻿// test.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
+// ConsoleApplication1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
 #include "pch.h"
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+void quickSort(vector<int> &num, int low, int high)
+{
+	if (low >= high) { return; }
+
+	int p = num[low];
+	int l = low, r = high;
+	while (l < r)
+	{
+		while (l < r && num[r] >= p)
+			--r;
+		num[l] = num[r];
+		while (l < r && num[l] <= p)
+			++l;
+		num[r] = num[l];
+	}
+	num[l] = p;
+
+	quickSort(num, low, l - 1);
+	quickSort(num, l + 1, high);
+}
+
+void pout(vector<int> num)
+{
+	for (auto n : num) {
+		cout << n << '\t';
+	}
+	cout << endl;
+
+	return;
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	vector<int> num{ 5, 7, 4, 3, 2, 1, 6 };
+
+	pout(num);
+	quickSort(num, 0, num.size() - 1);
+	pout(num);
+
+	std::cout << "Hello World!\n";
+	return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门提示: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
